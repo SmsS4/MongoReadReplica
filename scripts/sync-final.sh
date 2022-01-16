7cd7b47ff0f24bbd39b06a6bb618ebe02d4931ee
SmsS <smss.lite@gmail.com>
2022-01-17 01:54:06 +0330

    Split read and write

---
+++ scripts/sync-final.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+until mongo --host mongo-write:27017 --eval "print(\"waited for connection write\")"
+do
+    sleep 1
+done
+
+until mongo --host mongo-read:27018 --eval "print(\"waited for connection read\")"
+do
+    sleep 1
+done
+
+echo "Adding replicas"
+
+mongo --host mongo-write:27017 <<EOF
+var config = {
+    "_id": "dbrs",
+    "version": 1,
+    "members": [
+        {
+            "_id": 1,
+            "host": "mongo-write:27017",
+            "priority": 2
+        },
+        {
+            "_id": 2,
+            "host": "mongo-read:27018",
+            "priority": 1
+        }
+    ]
+};
+rs.initiate(config, { force: true });
+rs.reconfig(config, { force: true });
+EOF
